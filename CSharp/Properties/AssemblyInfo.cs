a9c278c7960b7c18011e4f317feb0b3af1a2f409
unknown <D:\The Bat!>
2019-08-14 11:42:02 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VintaSoft Ltd.")]
 [assembly: AssemblyProduct("TwainConsoleDemo")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2018")]
+[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.3.1.3")]
-[assembly: AssemblyFileVersion("10.3.1.3")]
+[assembly: AssemblyVersion("10.3.8.1")]
+[assembly: AssemblyFileVersion("10.3.8.1")]
