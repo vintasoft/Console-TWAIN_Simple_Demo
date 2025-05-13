aead22588b66fbfc16ea60049936e97d24b24a50
vintasoft <alex@vintasoft.com>
2025-05-13 13:47:21 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VintaSoft LLC")]
 [assembly: AssemblyProduct("TwainConsoleDemo")]
-[assembly: AssemblyCopyright("Copyright VintaSoft LLC 2024")]
+[assembly: AssemblyCopyright("Copyright VintaSoft LLC 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("15.1.1.1")]
-[assembly: AssemblyFileVersion("15.1.1.1")]
+[assembly: AssemblyVersion("15.1.2.1")]
+[assembly: AssemblyFileVersion("15.1.2.1")]
